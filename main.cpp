af988a7b80871e22029e7100b3b7cbda5ff23b5e
Amado-Developer <amado_developer@hotmail.com>
2020-10-24 13:20:33 -0600

    just last things

--- main.cpp
+++ main.cpp
@@ -43,216 +43,216 @@ int main()
     r.setScene(Sphere(make_tuple(4.0, 4.0, -15.0), 1.0), 0.0);
 //    r.setScene(Plane(make_tuple(0.0, -1.0, -10.0), make_tuple(0.0, 1.0, 0.1), leaf));
 
-    //Pig
+    //=========================================Pig======================================================
     //Head
-    r.setScene(Cube(make_tuple(-2.2, -0.1666666, -3.85), 0.5, pig));
+    r.setScene(Cube(make_tuple(-2.2, -1.1666666, -3.85), 0.5, pig));
 
     //Mouth
-    r.setScene(Cube(make_tuple(-2.2, -0.26666, -3.55), 0.1666666, pigMouth));
+    r.setScene(Cube(make_tuple(-2.2, -1.26666, -3.55), 0.1666666, pigMouth));
 
     //Body
-    r.setScene(Cube(make_tuple(-2.2, -0.666666, -4.5), 0.5, pig));
-    r.setScene(Cube(make_tuple(-2.2, -0.666666, -4.0), 0.5, pig));
+    r.setScene(Cube(make_tuple(-2.2, -1.666666, -4.5), 0.5, pig));
+    r.setScene(Cube(make_tuple(-2.2, -1.666666, -4.0), 0.5, pig));
 
     //Legs
     //Left Leg
-    r.setScene(Cube(make_tuple(-2.38, -0.95, -3.85), 0.1666666, pig));
-    r.setScene(Cube(make_tuple(-2.38, -1.1, -3.85), 0.1666666, pig));
+    r.setScene(Cube(make_tuple(-2.38, -1.95, -3.85), 0.1666666, pig));
+    r.setScene(Cube(make_tuple(-2.38, -2.1, -3.85), 0.1666666, pig));
 
     //Right Leg
-    r.setScene(Cube(make_tuple(-2.040, -0.95, -3.85), 0.1666666, pig));
-    r.setScene(Cube(make_tuple(-2.040, -1.1, -3.85), 0.1666666, pig));
+    r.setScene(Cube(make_tuple(-2.040, -1.95, -3.85), 0.1666666, pig));
+    r.setScene(Cube(make_tuple(-2.040, -2.1, -3.85), 0.1666666, pig));
 
     //Right Back Leg
-    r.setScene(Cube(make_tuple(-2.040, -0.95, -4.70), 0.1666666, pig));
-    r.setScene(Cube(make_tuple(-2.040, -1.1, -4.70), 0.1666666, pig));
+    r.setScene(Cube(make_tuple(-2.040, -1.95, -4.70), 0.1666666, pig));
+    r.setScene(Cube(make_tuple(-2.040, -2.1, -4.70), 0.1666666, pig));
 
     //Eyes
     //Left eye
-    r.setScene(Cube(make_tuple(-2.4, -0.066666, -3.6), 0.0625, blackEye));
-    r.setScene(Cube(make_tuple(-2.3375,-0.066666, -3.6), 0.0625, whiteEye));
+    r.setScene(Cube(make_tuple(-2.4, -1.066666, -3.6), 0.0625, blackEye));
+    r.setScene(Cube(make_tuple(-2.3375,-1.066666, -3.6), 0.0625, whiteEye));
 
     //Right eye
-    r.setScene(Cube(make_tuple(-1.99, -0.066666, -3.6), 0.0625, blackEye));
-    r.setScene(Cube(make_tuple(-2.0525,  -0.066666, -3.6), 0.0625, whiteEye));
+    r.setScene(Cube(make_tuple(-1.99, -1.066666, -3.6), 0.0625, blackEye));
+    r.setScene(Cube(make_tuple(-2.0525,  -1.066666, -3.6), 0.0625, whiteEye));
 
  //   ====================================Sheep========================================================
     //Head
-    r.setScene(Cube(make_tuple(0.0, -0.1666666, -3.85), 0.5, wool));
+    r.setScene(Cube(make_tuple(0.0, -1.1666666, -4.35), 0.5, wool));
 
     //Face
-    r.setScene(Cube(make_tuple(-0.11, -0.15, -3.55), 0.25, brownWool));
-    r.setScene(Cube(make_tuple(0.11, -0.15, -3.55), 0.25, brownWool));
-    r.setScene(Cube(make_tuple(0.0, -0.265, -3.55), 0.25, brownWool));
+    r.setScene(Cube(make_tuple(-0.11, -1.15, -4.15), 0.25, brownWool));
+    r.setScene(Cube(make_tuple(0.11, -1.15, -4.15), 0.25, brownWool));
+    r.setScene(Cube(make_tuple(0.0, -1.265, -4.15), 0.25, brownWool));
     //Mouth
-    r.setScene(Cube(make_tuple(0.0, -0.31, -3.50), 0.1666666, pigMouth));
+    r.setScene(Cube(make_tuple(0.0, -1.31, -4.00), 0.1666666, pigMouth));
 
     //Body
-    r.setScene(Cube(make_tuple(0.0, -0.666666, -4.0), 0.5, wool));
+    r.setScene(Cube(make_tuple(0.0, -1.666666, -4.4), 0.5, wool));
 
     //Legs
     //Left Leg
-    r.setScene(Cube(make_tuple(-0.168, -0.95, -3.85), 0.1666666, pig));
-    r.setScene(Cube(make_tuple(-0.168, -1.1, -3.85), 0.1666666, brownWool));
+    r.setScene(Cube(make_tuple(-0.168, -1.95, -4.35), 0.1666666, pig));
+    r.setScene(Cube(make_tuple(-0.168, -2.1, -4.35), 0.1666666, brownWool));
 
-//    Right Leg
-    r.setScene(Cube(make_tuple(0.168, -0.95, -3.85), 0.1666666, pig));
-    r.setScene(Cube(make_tuple(0.168, -1.1, -3.85), 0.1666666, brownWool));
+    //Right Leg
+    r.setScene(Cube(make_tuple(0.168, -1.95, -4.35), 0.1666666, pig));
+    r.setScene(Cube(make_tuple(0.168, -2.1, -4.35), 0.1666666, brownWool));
 
     //Eyes
     //Left eye
-    r.setScene(Cube(make_tuple(-0.19, -0.066666, -3.25), 0.0625, blackEye));
-    r.setScene(Cube(make_tuple(-0.1275,-0.066666, -3.25), 0.0625, whiteEye));
+    r.setScene(Cube(make_tuple(-0.19, -1.066666, -3.75), 0.0625, blackEye));
+    r.setScene(Cube(make_tuple(-0.1275,-1.066666, -3.75), 0.0625, whiteEye));
 
     //Right eye
-    r.setScene(Cube(make_tuple(0.19, -0.066666, -3.25), 0.0625, blackEye));
-    r.setScene(Cube(make_tuple(0.1275,  -0.066666, -3.25), 0.0625, whiteEye));
+    r.setScene(Cube(make_tuple(0.19, -1.066666, -3.75), 0.0625, blackEye));
+    r.setScene(Cube(make_tuple(0.1275,  -1.066666, -3.75), 0.0625, whiteEye));
 
     //======================================Creeper=============================================================
     //Head
-    r.setScene(Cube(make_tuple(1.8, -0.666666, -4.0), 1.0, creeper1));
+    r.setScene(Cube(make_tuple(1.8, -1.766666, -4.55), 1.0, creeper1));
 
     //Face
     //Eyes
-    r.setScene(Cube(make_tuple(1.6, -0.5, -3.55), 0.20, blackEye));
-    r.setScene(Cube(make_tuple(2.04, -0.5, -3.55), 0.20, blackEye));
-    r.setScene(Cube(make_tuple(1.830, -0.71, -3.55), 0.20, blackEye));
+    r.setScene(Cube(make_tuple(1.6, -1.605, -4.14), 0.20, blackEye));
+    r.setScene(Cube(make_tuple(2.0, -1.605, -4.14), 0.20, blackEye));
+    r.setScene(Cube(make_tuple(1.8, -1.81, -4.14), 0.20, blackEye));
 
     //Mouth?
-    r.setScene(Cube(make_tuple(1.830, -0.81, -3.55), 0.20, blackEye));
+    r.setScene(Cube(make_tuple(1.8, -1.91, -4.14), 0.20, blackEye));
 
     //left Mouth?
-    r.setScene(Cube(make_tuple(1.67, -0.729, -3.53), 0.1, blackEye));
-    r.setScene(Cube(make_tuple(1.67, -0.829, -3.53), 0.1, blackEye));
-    r.setScene(Cube(make_tuple(1.67, -0.929, -3.53), 0.1, blackEye));
+    r.setScene(Cube(make_tuple(1.645, -1.859, -4.09), 0.1, blackEye));
+    r.setScene(Cube(make_tuple(1.645, -1.959, -4.09), 0.1, blackEye));
+    r.setScene(Cube(make_tuple(1.645, -2.059, -4.09), 0.1, blackEye));
 
     //Right Mouth?
-    r.setScene(Cube(make_tuple(1.994, -0.729, -3.53), 0.1, blackEye));
-    r.setScene(Cube(make_tuple(1.994, -0.829, -3.53), 0.1, blackEye));
-    r.setScene(Cube(make_tuple(1.994, -0.929, -3.53), 0.1, blackEye));
+    r.setScene(Cube(make_tuple(1.95, -1.859, -4.095), 0.1, blackEye));
+    r.setScene(Cube(make_tuple(1.95, -1.959, -4.095), 0.1, blackEye));
+    r.setScene(Cube(make_tuple(1.95, -2.059, -4.095), 0.1, blackEye));
 
 
-    //==================================Letter A================================================================
-//    //Left
-//    r.setScene(Cube(make_tuple(-3.0, -1.0, -5.0), 0.25, leaf));
-//    r.setScene(Cube(make_tuple(-2.95, -0.75, -5.0),0.25, leaf));
-//    r.setScene(Cube(make_tuple(-2.90, -0.50, -5.0), 0.25, leaf));
-//    r.setScene(Cube(make_tuple(-2.85, -0.25, -5.0), 0.25, leaf));
-//    r.setScene(Cube(make_tuple(-2.80, 0.0, -5.0), 0.25, leaf));
-//    r.setScene(Cube(make_tuple(-2.70, 0.25, -5.0), 0.25, leaf));
-//
-//    //Center
-//    r.setScene(Cube(make_tuple(-2.55, -0.50, -5.0), 0.25, leaf));
-//    r.setScene(Cube(make_tuple(-2.65, -0.50, -5.0), 0.25, leaf));
-//    r.setScene(Cube(make_tuple(-2.45, -0.50, -5.0), 0.25, leaf));
-//
-//    //Top
-//    r.setScene(Cube(make_tuple(-2.60, 0.25, -5.0), 0.25, leaf));
-//    r.setScene(Cube(make_tuple(-2.50, 0.25, -5.0), 0.25, leaf));
-//    r.setScene(Cube(make_tuple(-2.40, 0.25, -5.0), 0.25, leaf));
-//
-//    //Right
-//    r.setScene(Cube(make_tuple(-2.40, 0.0, -5.0),0.25, leaf));
-//    r.setScene(Cube(make_tuple(-2.35, -0.25, -5.0), 0.25, leaf));
-//    r.setScene(Cube(make_tuple(-2.30, -0.5, -5.0), 0.25, leaf));
-//    r.setScene(Cube(make_tuple(-2.25, -0.75, -5.0), 0.25, leaf));
-//    r.setScene(Cube(make_tuple(-2.20, -1.0, -5.0), 0.25, leaf));
-//
-//    //Letter M
-//    //Left
-//    r.setScene(Cube(make_tuple(-1.9, -1.0, -5.0), 0.25, leaf));
-//    r.setScene(Cube(make_tuple(-1.85, -0.75, -5.0),0.25, leaf));
-//    r.setScene(Cube(make_tuple(-1.80, -0.50, -5.0), 0.25, leaf));
-//    r.setScene(Cube(make_tuple(-1.75, -0.25, -5.0), 0.25, leaf));
-//    r.setScene(Cube(make_tuple(-1.70, 0.0, -5.0), 0.25, leaf));
-//    r.setScene(Cube(make_tuple(-1.60, 0.25, -5.0), 0.25, leaf));
-//
-//    //Center
-//    r.setScene(Cube(make_tuple(-1.445, 0.0, -5.0),0.25, leaf));
-//    r.setScene(Cube(make_tuple(-1.05, 0.0, -5.0),0.25, leaf));
-//    r.setScene(Cube(make_tuple(-1.25, -0.25, -5.0),0.25, leaf));
-//
-//    //Right
-//    r.setScene(Cube(make_tuple(-0.85, 0.25, -5.0),0.25, leaf));
-//    r.setScene(Cube(make_tuple(-0.80, 0.0, -5.0),0.25, leaf));
-//    r.setScene(Cube(make_tuple(-0.75, -0.25, -5.0), 0.25, leaf));
-//    r.setScene(Cube(make_tuple(-0.70, -0.5, -5.0), 0.25, leaf));
-//    r.setScene(Cube(make_tuple(-0.65, -0.75, -5.0), 0.25, leaf));
-//    r.setScene(Cube(make_tuple(-0.60, -1.0, -5.0), 0.25, leaf));
-//
-//    //Letter A(2)
-//    //Left
-//    r.setScene(Cube(make_tuple(-0.30, -1.0, -5.0), 0.25, nose));
-//    r.setScene(Cube(make_tuple(-0.25, -0.75, -5.0),0.25, nose));
-//    r.setScene(Cube(make_tuple(-0.20, -0.50, -5.0), 0.25, nose));
-//    r.setScene(Cube(make_tuple(-0.15, -0.25, -5.0), 0.25, nose));
-//    r.setScene(Cube(make_tuple(-0.10, 0.0, -5.0), 0.25, nose));
-//    r.setScene(Cube(make_tuple(-0.00, 0.25, -5.0), 0.25, nose));
-//
-//    //Center
-//    r.setScene(Cube(make_tuple(-0.05, -0.50, -5.0), 0.25, nose));
-//    r.setScene(Cube(make_tuple(0.20, -0.50, -5.0), 0.25, nose));
-//    r.setScene(Cube(make_tuple(0.45, -0.50, -5.0), 0.25, nose));
-//
-//    //Top
-//    r.setScene(Cube(make_tuple(0.25, 0.25, -5.0), 0.25, nose));
-//    r.setScene(Cube(make_tuple(0.50, 0.25, -5.0), 0.25, nose));
-//
-//    //Right
-//    r.setScene(Cube(make_tuple(0.60, 0.0, -5.0),0.25, nose));
-//    r.setScene(Cube(make_tuple(0.55, -0.25, -5.0), 0.25, nose));
-//    r.setScene(Cube(make_tuple(0.50, -0.5, -5.0), 0.25, nose));
-//    r.setScene(Cube(make_tuple(0.45, -0.75, -5.0), 0.25, nose));
-//    r.setScene(Cube(make_tuple(0.40, -1.0, -5.0), 0.25, nose));
-//
-//    //Letter D
-//    //Left
-//    r.setScene(Cube(make_tuple(0.90, -1.0, -5.0), 0.25, antennae));
-//    r.setScene(Cube(make_tuple(0.90, -0.75, -5.0), 0.25, antennae));
-//    r.setScene(Cube(make_tuple(0.90, -0.50, -5.0), 0.25, antennae));
-//    r.setScene(Cube(make_tuple(0.90, -0.25, -5.0), 0.25, antennae));
-//    r.setScene(Cube(make_tuple(0.90, -0.0, -5.0), 0.25, antennae));
-//    r.setScene(Cube(make_tuple(0.90,  0.25, -5.0), 0.25, antennae));
-//    r.setScene(Cube(make_tuple(1.15,  0.25, -5.0), 0.25, antennae));
-//    r.setScene(Cube(make_tuple(1.40,  0.15, -5.0), 0.25, antennae));
-//    r.setScene(Cube(make_tuple(1.65,  0.05, -5.0), 0.25, antennae));
-//
-//
-//    //Right
-//    r.setScene(Cube(make_tuple(1.65, -0.20, -5.0), 0.25, antennae));
-//    r.setScene(Cube(make_tuple(1.65, -0.30, -5.0), 0.25, antennae));
-//    r.setScene(Cube(make_tuple(1.65, -0.55, -5.0), 0.25, antennae));
-//    r.setScene(Cube(make_tuple(1.65, -0.7, -5.0), 0.25, antennae));
-//
-//
-//    r.setScene(Cube(make_tuple(1.65, -0.8, -5.0), 0.25, antennae));
-//    r.setScene(Cube(make_tuple(1.40, -0.9, -5.0), 0.25, antennae));
-//    r.setScene(Cube(make_tuple(1.15, -1.0, -5.0), 0.25, antennae));
-//
-//    //Letter O
-//    //Left
-////    r.setScene(Cube(make_tuple(1.95, -1.0, -5.0), 0.25, eye));
-//    r.setScene(Cube(make_tuple(1.95, -0.75, -5.0), 0.25, eye));
-//    r.setScene(Cube(make_tuple(1.95, -0.50, -5.0), 0.25, eye));
-//    r.setScene(Cube(make_tuple(1.95, -0.25, -5.0), 0.25, eye));
-//    r.setScene(Cube(make_tuple(1.95,   0.0, -5.0), 0.25, eye));
-//
-//    //Top
-//    r.setScene(Cube(make_tuple(2.20,   0.25, -5.0), 0.25, eye));
-//    r.setScene(Cube(make_tuple(2.45,   0.25, -5.0), 0.25, eye));
-//    r.setScene(Cube(make_tuple(2.70,   0.25, -5.0), 0.25, eye));
-//
-//    //Bot
-//    r.setScene(Cube(make_tuple(2.20, -1.0, -5.0), 0.25, eye));
-//    r.setScene(Cube(make_tuple(2.45, -1.0, -5.0), 0.25, eye));
-//    r.setScene(Cube(make_tuple(2.70, -1.0, -5.0), 0.25, eye));
-//
-//    //Right
-////    r.setScene(Cube(make_tuple(2.95, -1.0, -5.0), 0.25,  eye));
-//    r.setScene(Cube(make_tuple(2.95, -0.75, -5.0), 0.25, eye));
-//    r.setScene(Cube(make_tuple(2.95, -0.50, -5.0), 0.25, eye));
-//    r.setScene(Cube(make_tuple(2.95, -0.25, -5.0), 0.25, eye));
-//    r.setScene(Cube(make_tuple(2.95,   0.0, -5.0), 0.25, eye));
+//    ==================================Letter A================================================================
+    //Left
+    r.setScene(Cube(make_tuple(-3.0, -1.0, -5.0), 0.25, leaf));
+    r.setScene(Cube(make_tuple(-2.95, -0.75, -5.0),0.25, leaf));
+    r.setScene(Cube(make_tuple(-2.90, -0.50, -5.0), 0.25, leaf));
+    r.setScene(Cube(make_tuple(-2.85, -0.25, -5.0), 0.25, leaf));
+    r.setScene(Cube(make_tuple(-2.80, 0.0, -5.0), 0.25, leaf));
+    r.setScene(Cube(make_tuple(-2.70, 0.25, -5.0), 0.25, leaf));
+
+    //Center
+    r.setScene(Cube(make_tuple(-2.55, -0.50, -5.0), 0.25, leaf));
+    r.setScene(Cube(make_tuple(-2.65, -0.50, -5.0), 0.25, leaf));
+    r.setScene(Cube(make_tuple(-2.45, -0.50, -5.0), 0.25, leaf));
+
+    //Top
+    r.setScene(Cube(make_tuple(-2.60, 0.25, -5.0), 0.25, leaf));
+    r.setScene(Cube(make_tuple(-2.50, 0.25, -5.0), 0.25, leaf));
+    r.setScene(Cube(make_tuple(-2.40, 0.25, -5.0), 0.25, leaf));
+
+    //Right
+    r.setScene(Cube(make_tuple(-2.40, 0.0, -5.0),0.25, leaf));
+    r.setScene(Cube(make_tuple(-2.35, -0.25, -5.0), 0.25, leaf));
+    r.setScene(Cube(make_tuple(-2.30, -0.5, -5.0), 0.25, leaf));
+    r.setScene(Cube(make_tuple(-2.25, -0.75, -5.0), 0.25, leaf));
+    r.setScene(Cube(make_tuple(-2.20, -1.0, -5.0), 0.25, leaf));
+
+    //Letter M
+    //Left
+    r.setScene(Cube(make_tuple(-1.9, -1.0, -5.0), 0.25, leaf));
+    r.setScene(Cube(make_tuple(-1.85, -0.75, -5.0),0.25, leaf));
+    r.setScene(Cube(make_tuple(-1.80, -0.50, -5.0), 0.25, leaf));
+    r.setScene(Cube(make_tuple(-1.75, -0.25, -5.0), 0.25, leaf));
+    r.setScene(Cube(make_tuple(-1.70, 0.0, -5.0), 0.25, leaf));
+    r.setScene(Cube(make_tuple(-1.60, 0.25, -5.0), 0.25, leaf));
+
+    //Center
+    r.setScene(Cube(make_tuple(-1.445, 0.0, -5.0),0.25, leaf));
+    r.setScene(Cube(make_tuple(-1.05, 0.0, -5.0),0.25, leaf));
+    r.setScene(Cube(make_tuple(-1.25, -0.25, -5.0),0.25, leaf));
+
+    //Right
+    r.setScene(Cube(make_tuple(-0.85, 0.25, -5.0),0.25, leaf));
+    r.setScene(Cube(make_tuple(-0.80, 0.0, -5.0),0.25, leaf));
+    r.setScene(Cube(make_tuple(-0.75, -0.25, -5.0), 0.25, leaf));
+    r.setScene(Cube(make_tuple(-0.70, -0.5, -5.0), 0.25, leaf));
+    r.setScene(Cube(make_tuple(-0.65, -0.75, -5.0), 0.25, leaf));
+    r.setScene(Cube(make_tuple(-0.60, -1.0, -5.0), 0.25, leaf));
+
+    //Letter A(2)
+    //Left
+    r.setScene(Cube(make_tuple(-0.30, -1.0, -5.0), 0.25, nose));
+    r.setScene(Cube(make_tuple(-0.25, -0.75, -5.0),0.25, nose));
+    r.setScene(Cube(make_tuple(-0.20, -0.50, -5.0), 0.25, nose));
+    r.setScene(Cube(make_tuple(-0.15, -0.25, -5.0), 0.25, nose));
+    r.setScene(Cube(make_tuple(-0.10, 0.0, -5.0), 0.25, nose));
+    r.setScene(Cube(make_tuple(-0.00, 0.25, -5.0), 0.25, nose));
+
+    //Center
+    r.setScene(Cube(make_tuple(-0.05, -0.50, -5.0), 0.25, nose));
+    r.setScene(Cube(make_tuple(0.20, -0.50, -5.0), 0.25, nose));
+    r.setScene(Cube(make_tuple(0.45, -0.50, -5.0), 0.25, nose));
+
+    //Top
+    r.setScene(Cube(make_tuple(0.25, 0.25, -5.0), 0.25, nose));
+    r.setScene(Cube(make_tuple(0.50, 0.25, -5.0), 0.25, nose));
+
+    //Right
+    r.setScene(Cube(make_tuple(0.60, 0.0, -5.0),0.25, nose));
+    r.setScene(Cube(make_tuple(0.55, -0.25, -5.0), 0.25, nose));
+    r.setScene(Cube(make_tuple(0.50, -0.5, -5.0), 0.25, nose));
+    r.setScene(Cube(make_tuple(0.45, -0.75, -5.0), 0.25, nose));
+    r.setScene(Cube(make_tuple(0.40, -1.0, -5.0), 0.25, nose));
+
+    //Letter D
+    //Left
+    r.setScene(Cube(make_tuple(0.90, -1.0, -5.0), 0.25, antennae));
+    r.setScene(Cube(make_tuple(0.90, -0.75, -5.0), 0.25, antennae));
+    r.setScene(Cube(make_tuple(0.90, -0.50, -5.0), 0.25, antennae));
+    r.setScene(Cube(make_tuple(0.90, -0.25, -5.0), 0.25, antennae));
+    r.setScene(Cube(make_tuple(0.90, -0.0, -5.0), 0.25, antennae));
+    r.setScene(Cube(make_tuple(0.90,  0.25, -5.0), 0.25, antennae));
+    r.setScene(Cube(make_tuple(1.15,  0.25, -5.0), 0.25, antennae));
+    r.setScene(Cube(make_tuple(1.40,  0.15, -5.0), 0.25, antennae));
+    r.setScene(Cube(make_tuple(1.65,  0.05, -5.0), 0.25, antennae));
+
+
+    //Right
+    r.setScene(Cube(make_tuple(1.65, -0.20, -5.0), 0.25, antennae));
+    r.setScene(Cube(make_tuple(1.65, -0.30, -5.0), 0.25, antennae));
+    r.setScene(Cube(make_tuple(1.65, -0.55, -5.0), 0.25, antennae));
+    r.setScene(Cube(make_tuple(1.65, -0.7, -5.0), 0.25, antennae));
+
+
+    r.setScene(Cube(make_tuple(1.65, -0.8, -5.0), 0.25, antennae));
+    r.setScene(Cube(make_tuple(1.40, -0.9, -5.0), 0.25, antennae));
+    r.setScene(Cube(make_tuple(1.15, -1.0, -5.0), 0.25, antennae));
+
+    //Letter O
+    //Left
+//    r.setScene(Cube(make_tuple(1.95, -1.0, -5.0), 0.25, eye));
+    r.setScene(Cube(make_tuple(1.95, -0.75, -5.0), 0.25, eye));
+    r.setScene(Cube(make_tuple(1.95, -0.50, -5.0), 0.25, eye));
+    r.setScene(Cube(make_tuple(1.95, -0.25, -5.0), 0.25, eye));
+    r.setScene(Cube(make_tuple(1.95,   0.0, -5.0), 0.25, eye));
+
+    //Top
+    r.setScene(Cube(make_tuple(2.20,   0.25, -5.0), 0.25, eye));
+    r.setScene(Cube(make_tuple(2.45,   0.25, -5.0), 0.25, eye));
+    r.setScene(Cube(make_tuple(2.70,   0.25, -5.0), 0.25, eye));
+
+    //Bot
+    r.setScene(Cube(make_tuple(2.20, -1.0, -5.0), 0.25, eye));
+    r.setScene(Cube(make_tuple(2.45, -1.0, -5.0), 0.25, eye));
+    r.setScene(Cube(make_tuple(2.70, -1.0, -5.0), 0.25, eye));
+
+    //Right
+//    r.setScene(Cube(make_tuple(2.95, -1.0, -5.0), 0.25,  eye));
+    r.setScene(Cube(make_tuple(2.95, -0.75, -5.0), 0.25, eye));
+    r.setScene(Cube(make_tuple(2.95, -0.50, -5.0), 0.25, eye));
+    r.setScene(Cube(make_tuple(2.95, -0.25, -5.0), 0.25, eye));
+    r.setScene(Cube(make_tuple(2.95,   0.0, -5.0), 0.25, eye));
 
     r.render();
     r.glFinish("proyecto.bmp");
